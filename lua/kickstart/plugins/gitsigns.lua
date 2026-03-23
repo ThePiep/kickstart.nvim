476cfcdfac81ab94256fa201abeeedb13ad11f1e
ThePiep <kaj.rijn@gmail.com>
2026-03-23 19:56:17 +0100

    Enable gitsigns recommended keymaps and customize

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -46,11 +46,14 @@ return {
       map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
       map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
       map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-      map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+      map('n', '<leader>hP', gitsigns.preview_hunk, { desc = 'git [P]review hunk' })
+      map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'git preview hunk [i]nline' })
       map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
       map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
       map('n', '<leader>hD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit' })
-      -- Toggles
+      map('n', '<leader>hp', function() gitsigns.nav_hunk 'prev' end, { desc = 'git [p]rev hunk' })
+      map('n', '<leader>hn', function() gitsigns.nav_hunk 'next' end, { desc = 'git [n]ext hunk' })
+      -- Toggle s
       map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
       map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
     end,
