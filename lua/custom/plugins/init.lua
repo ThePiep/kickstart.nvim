060107c4f7e4ee2e22b6e4b6a1e2e7ab91b6dda2
Kaj van Rijn <kaj.rijn@gmail.nl>
2026-03-26 18:08:12 +0100

    Add recommended autosession setting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,6 +17,10 @@ return {
       suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
       -- log_level = 'debug',
     },
+    config = function()
+      vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+      require('auto-session').setup()
+    end,
   },
   {
     'NeogitOrg/neogit',
